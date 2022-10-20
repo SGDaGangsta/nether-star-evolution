910e238058ecfbefac3cd85e9ad9ac628699e4d7
Sander Geyssens <sander.geyssens@student.odisee.be>
2022-10-20 03:52:27 +0200

    initial setup of project

---
+++ src/main/kotlin/net/sgdagangsta/netherstarevo/NetherStarEvolution.kt
@@ -0,0 +1,11 @@
+package net.sgdagangsta.netherstarevo
+
+import net.fabricmc.api.ModInitializer
+
+object NetherStarEvolution : ModInitializer {
+    private const val MOD_ID = "netherstarevo"
+
+    override fun onInitialize() {
+
+    }
+}
